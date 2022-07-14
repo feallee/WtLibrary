72d81a49a95e351b9b457c2bfe0f1d7d62799c39
feallee <feallee@hotmail.com>
2022-07-14 11:21:40 +0800

    清理解决方案。

--- Queue/Queue.h
+++
@@ -1,8 +0,0 @@
-#ifndef __QUEUE_H_
-#define __QUEUE_H_
-#define QUEUE_TYPE int //可以自定义
-void* Queue_Create(void);
-void Queue_Push(void* queue, QUEUE_TYPE payload);
-QUEUE_TYPE Queue_Pop(void* queue);
-void Queue_Dispose(void* queue);
-#endif
