619cc0ad51470c667895df4e926c0c76f4c23dd3
feallee <feallee@hotmail.com>
2024-06-21 16:13:54 +0800

    增加注释。

--- applications/WtGSensor.h
+++ applications/WtGSensor.h
@@ -1,6 +1,11 @@
 #ifndef __WT_GSENSOR_H_
 #define __WT_GSENSOR_H_
-
+/// @brief 根据 GSensor 的三轴加速度数据计算pitch和roll角度。
+/// @param[in] x X轴加速度数据。
+/// @param[in] y Y轴加速度数据。
+/// @param[in] z Z轴加速度数据。
+/// @param[out] pitch 俯仰角度。
+/// @param[out] roll 翻滚角度。
 extern void WtGSensor_ComputePitchRoll(double x, double y, double z, double *pitch, double *roll);
 
 #endif
--- applications/WtVersion.h
+++ applications/WtVersion.h
@@ -10,46 +10,46 @@
 #define __WT_VERSION_H_
 #include "WtBuild.h"
 
-/// @brief 产品名称。
-#define WT_VERSION_PRODUCT_NAME "LGL" // TODO: 产品名称
-/// @brief 网络制式。
-#define WT_VERSION_PRODUCT_NETWORK "4G" // TODO: 网络制式，如 2G/3G/4G/5G/6G/NB/CAT1/CAT4/...
-/// @brief 客户标识。
-#define WT_VERSION_PRODUCT_CUSTOMER "MOBI" // TODO: 客户标识，如 QTK/ELC/MOBI/...
-/// @brief 功能特性。
-#define WT_VERSION_PRODUCT_FUNCTION "General" // TODO: 功能特性，如 General/Alarm/Security/...
-/// @brief 通信协议。
-#define WT_VERSION_PRODUCT_PROTOCOL "PT" // TODO: 通信协议，如 PT/JT808/...
+/// @brief 产品名称。// TODO
+#define WT_VERSION_PRODUCT_NAME "LGL"
+/// @brief 网络制式。如 2G/3G/4G/5G/6G/NB/CAT1/CAT4/... // TODO
+#define WT_VERSION_PRODUCT_NETWORK "4G"
+/// @brief 客户标识。如 QTK/ELC/MOBI/... // TODO
+#define WT_VERSION_PRODUCT_CUSTOMER "MOBI"
+/// @brief 功能特性。如 General/Alarm/Security/... // TODO
+#define WT_VERSION_PRODUCT_FUNCTION "General"
+/// @brief 通信协议。如 PT/JT808/... // TODO
+#define WT_VERSION_PRODUCT_PROTOCOL "PT"
 
-/// @brief 主要版本号。
-#define WT_VERSION_MAJOR 1 // TODO: 0 to 4293
-/// @brief 次要版本号。
-#define WT_VERSION_MINOR 0 // TODO: 0 to 999
-/// @brief 修订版本号。
-#define WT_VERSION_REVISION 1 // TODO: 0 to 999
+/// @brief 主要版本号。如 0 to 999。// TODO
+#define WT_VERSION_MAJOR 1
+/// @brief 次要版本号。如 0 to 999。// TODO
+#define WT_VERSION_MINOR 0
+/// @brief 修订版本号。如 0 to 999。// TODO
+#define WT_VERSION_REVISION 1
 
 #define WT_VERSION_TO_TEXT(v) #v
 #define WT_VERSION_TO_TEXT3(a, b, c) WT_VERSION_TO_TEXT(a.b.c)
 #define WT_VERSION_TO_TEXT1(a) WT_VERSION_TO_TEXT(a)
-#define WT_VERSION_BUILD_DATE             \
-    WT_VERSION_TO_TEXT1(WT_BUILD_YEAR_2)  \
-    WT_VERSION_TO_TEXT1(WT_BUILD_YEAR_1)  \
-    WT_VERSION_TO_TEXT1(WT_BUILD_MONTH_2) \
-    WT_VERSION_TO_TEXT1(WT_BUILD_MONTH_1) \
-    WT_VERSION_TO_TEXT1(WT_BUILD_DAY_2)   \
-    WT_VERSION_TO_TEXT1(WT_BUILD_DAY_1) \
-    WT_VERSION_TO_TEXT1(WT_BUILD_HOUR_2) \
-    WT_VERSION_TO_TEXT1(WT_BUILD_HOUR_1) \
+#define WT_VERSION_BUILD_DATE              \
+    WT_VERSION_TO_TEXT1(WT_BUILD_YEAR_2)   \
+    WT_VERSION_TO_TEXT1(WT_BUILD_YEAR_1)   \
+    WT_VERSION_TO_TEXT1(WT_BUILD_MONTH_2)  \
+    WT_VERSION_TO_TEXT1(WT_BUILD_MONTH_1)  \
+    WT_VERSION_TO_TEXT1(WT_BUILD_DAY_2)    \
+    WT_VERSION_TO_TEXT1(WT_BUILD_DAY_1)    \
+    WT_VERSION_TO_TEXT1(WT_BUILD_HOUR_2)   \
+    WT_VERSION_TO_TEXT1(WT_BUILD_HOUR_1)   \
     WT_VERSION_TO_TEXT1(WT_BUILD_MINUTE_2) \
     WT_VERSION_TO_TEXT1(WT_BUILD_MINUTE_1) \
     WT_VERSION_TO_TEXT1(WT_BUILD_SECOND_2) \
     WT_VERSION_TO_TEXT1(WT_BUILD_SECOND_1)
 
-/// @brief 版本号标量数值。如：1000001。
+/// @brief 版本号标量数值，如 1000001。
 #define WT_VERSION_NUMBER WT_VERSION_MAJOR * 1000000 + WT_VERSION_MINOR * 1000 + WT_VERSION_REVISION
-/// @brief 版本号紧凑字符串。如：V1.0.1。
+/// @brief 版本号紧凑字符串，如 V1.0.1。
 #define WT_VERSION_TINY "V" WT_VERSION_TO_TEXT3(WT_VERSION_MAJOR, WT_VERSION_MINOR, WT_VERSION_REVISION)
-/// @brief 版本号完整字符串。如：LGL_240311_4G_MOBI_General_PT_V1.0.1。
+/// @brief 版本号完整字符串，如 LGL_240311_4G_MOBI_General_PT_V1.0.1。
 #define WT_VERSION_FULL \
     WT_VERSION_PRODUCT_NAME "_" WT_VERSION_BUILD_DATE "_" WT_VERSION_PRODUCT_NETWORK "_" WT_VERSION_PRODUCT_CUSTOMER "_" WT_VERSION_PRODUCT_FUNCTION "_" WT_VERSION_PRODUCT_PROTOCOL "_" WT_VERSION_TINY
 #endif
